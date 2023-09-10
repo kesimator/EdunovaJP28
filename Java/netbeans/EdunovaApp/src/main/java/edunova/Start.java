06e27ff9bbb192a3b84e0bb9787eb4bff13d6a1d
kesimator <132385476+kesimator@users.noreply.github.com>
2023-09-10 15:21:21 +0200

    Update Start.java

--- Java/netbeans/EdunovaApp/src/main/java/edunova/Start.java
+++ Java/netbeans/EdunovaApp/src/main/java/edunova/Start.java
@@ -1,7 +1,6 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  */
-
 package edunova;
 
 import com.google.gson.Gson;
@@ -27,14 +26,10 @@ import org.hibernate.Session;
 public class Start {
 
     public static void main(String[] args) {
-       
+
         // HibernateUtil.getSession();
         // new PocetniInsert();
-        
         // System.out.println(new Gson().toJson(new ObradaSmjer().read()));
-        
-        
-        
 //        ObradaSmjer os = new ObradaSmjer();
 //        Smjer s = new Smjer();
 //        s.setNaziv("Prvi iz koda");
@@ -45,7 +40,6 @@ public class Start {
 //        } catch (EdunovaException e) {
 //            System.out.println(e.getPoruka());
 //        }
-           
 //           List<Smjer> smjerovi = new ObradaSmjer().read();
 //
 //           Smjer zadnjiSmjer = smjerovi.get(0);
@@ -57,50 +51,40 @@ public class Start {
 //        } catch (EdunovaException e) {
 //            e.printStackTrace();
 //        }
-           
-           
-           
-           ObradaPolaznik op = new ObradaPolaznik();
-           Polaznik p = new Polaznik();
-           p.setIme("Pero");
-           p.setPrezime("Peric");
-           p.setOib("54469438172");
-           
-           op.setEntitet(p);
-           
-           try {
+        ObradaPolaznik op = new ObradaPolaznik();
+        Polaznik p = new Polaznik();
+        p.setIme("Pero");
+        p.setPrezime("Peric");
+        p.setOib("54469438172");
+
+        op.setEntitet(p);
+
+        try {
             op.create();
         } catch (EdunovaException e) {
-               System.out.println(e.getPoruka());
+            System.out.println(e.getPoruka());
         }
-           
-        
-       
-        }
-    
-    
-    private void primjerJSON() {
 
-            String json = "[{\"naziv\":\"Delirium Tremens\",\"cijena\":923.00,\"upisnina\":90.00,\"trajanje\":180,\"verificiran\":true,\"sifra\":1},{\"naziv\":\"Sierra Nevada Celebration Ale\",\"cijena\":1044.00,\"upisnina\":62.00,\"trajanje\":262,\"verificiran\":true,\"sifra\":2},{\"naziv\":\"Oak Aged Yeti Imperial Stout\",\"cijena\":1096.00,\"upisnina\":68.00,\"trajanje\":270,\"verificiran\":false,\"sifra\":3},{\"naziv\":\"Sierra Nevada Bigfoot Barleywine Style Ale\",\"cijena\":1019.00,\"upisnina\":103.00,\"trajanje\":109,\"verificiran\":true,\"sifra\":4},{\"naziv\":\"HopSlam Ale\",\"cijena\":1368.00,\"upisnina\":55.00,\"trajanje\":134,\"verificiran\":false,\"sifra\":5},{\"naziv\":\"Yeti Imperial Stout\",\"cijena\":1528.00,\"upisnina\":108.00,\"trajanje\":316,\"verificiran\":false,\"sifra\":6},{\"naziv\":\"Shakespeare Oatmeal\",\"cijena\":1272.00,\"upisnina\":114.00,\"trajanje\":155,\"verificiran\":true,\"sifra\":7},{\"naziv\":\"Dreadnaught IPA\",\"cijena\":986.00,\"upisnina\":58.00,\"trajanje\":95,\"verificiran\":false,\"sifra\":8},{\"naziv\":\"Schneider Aventinus\",\"cijena\":1506.00,\"upisnina\":83.00,\"trajanje\":185,\"verificiran\":true,\"sifra\":9},{\"naziv\":\"Sierra Nevada Celebration Ale\",\"cijena\":1319.00,\"upisnina\":107.00,\"trajanje\":95,\"verificiran\":false,\"sifra\":10},{\"naziv\":\"Hop Rod Rye\",\"cijena\":1314.00,\"upisnina\":81.00,\"trajanje\":148,\"verificiran\":true,\"sifra\":11},{\"naziv\":\"Old Rasputin Russian Imperial Stout\",\"cijena\":1532.00,\"upisnina\":89.00,\"trajanje\":327,\"verificiran\":false,\"sifra\":12},{\"naziv\":\"Prvi preko kontrola\",\"verificiran\":false,\"sifra\":13}]";        
-
-            List<Smjer> lista = new Gson().fromJson(json, new TypeToken<List<Smjer>>() {}.getType());
-
-            ObradaSmjer os = new ObradaSmjer();
-
-            for(Smjer s : lista) {
-                os.setEntitet(s);
-                try {
-                    os.create();
-                } catch (EdunovaException ex) {
-                    System.out.println(ex.getPoruka());
-                }
-}
-
-       
-        
     }
 
-        
+    private void primjerJSON() {
+
+        String json = "[{\"naziv\":\"Delirium Tremens\",\"cijena\":923.00,\"upisnina\":90.00,\"trajanje\":180,\"verificiran\":true,\"sifra\":1},{\"naziv\":\"Sierra Nevada Celebration Ale\",\"cijena\":1044.00,\"upisnina\":62.00,\"trajanje\":262,\"verificiran\":true,\"sifra\":2},{\"naziv\":\"Oak Aged Yeti Imperial Stout\",\"cijena\":1096.00,\"upisnina\":68.00,\"trajanje\":270,\"verificiran\":false,\"sifra\":3},{\"naziv\":\"Sierra Nevada Bigfoot Barleywine Style Ale\",\"cijena\":1019.00,\"upisnina\":103.00,\"trajanje\":109,\"verificiran\":true,\"sifra\":4},{\"naziv\":\"HopSlam Ale\",\"cijena\":1368.00,\"upisnina\":55.00,\"trajanje\":134,\"verificiran\":false,\"sifra\":5},{\"naziv\":\"Yeti Imperial Stout\",\"cijena\":1528.00,\"upisnina\":108.00,\"trajanje\":316,\"verificiran\":false,\"sifra\":6},{\"naziv\":\"Shakespeare Oatmeal\",\"cijena\":1272.00,\"upisnina\":114.00,\"trajanje\":155,\"verificiran\":true,\"sifra\":7},{\"naziv\":\"Dreadnaught IPA\",\"cijena\":986.00,\"upisnina\":58.00,\"trajanje\":95,\"verificiran\":false,\"sifra\":8},{\"naziv\":\"Schneider Aventinus\",\"cijena\":1506.00,\"upisnina\":83.00,\"trajanje\":185,\"verificiran\":true,\"sifra\":9},{\"naziv\":\"Sierra Nevada Celebration Ale\",\"cijena\":1319.00,\"upisnina\":107.00,\"trajanje\":95,\"verificiran\":false,\"sifra\":10},{\"naziv\":\"Hop Rod Rye\",\"cijena\":1314.00,\"upisnina\":81.00,\"trajanje\":148,\"verificiran\":true,\"sifra\":11},{\"naziv\":\"Old Rasputin Russian Imperial Stout\",\"cijena\":1532.00,\"upisnina\":89.00,\"trajanje\":327,\"verificiran\":false,\"sifra\":12},{\"naziv\":\"Prvi preko kontrola\",\"verificiran\":false,\"sifra\":13}]";
+
+        List<Smjer> lista = new Gson().fromJson(json, new TypeToken<List<Smjer>>() {
+        }.getType());
+
+        ObradaSmjer os = new ObradaSmjer();
+
+        for (Smjer s : lista) {
+            os.setEntitet(s);
+            try {
+                os.create();
+            } catch (EdunovaException ex) {
+                System.out.println(ex.getPoruka());
+            }
+        }
+
+    }
+
 }
-
-
